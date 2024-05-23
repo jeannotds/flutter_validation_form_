198cd4963610ab51845fe0a0ed52659bffb5a206
diambu jeannot <diambujeannot@gmail.com>
2024-05-23 08:56:53 -0700

    create form

--- lib/pages/create_planning.dart
+++ lib/pages/create_planning.dart
@@ -10,9 +10,36 @@ class CreatePlanning extends StatefulWidget {
 class _CreatePlanningState extends State<CreatePlanning> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-        body: Center(
-      child: Text("Formulaire"),
+    return Scaffold(
+        body: Container(
+      padding: const EdgeInsets.only(top: 50),
+      child: Container(
+          padding: const EdgeInsets.all(20),
+          child: Form(
+              child: Column(
+            children: [
+              Container(
+                margin: const EdgeInsets.only(bottom: 10),
+                child: TextFormField(
+                  decoration: const InputDecoration(
+                      labelText: 'Nom',
+                      hintText: 'Entrer le nom de l utilisateur',
+                      border: OutlineInputBorder()),
+                ),
+              ),
+              SizedBox(
+                width: double.infinity,
+                height: 50,
+                child: ElevatedButton(
+                  onPressed: () {},
+                  style: const ButtonStyle(
+                      foregroundColor: MaterialStatePropertyAll(Colors.white),
+                      backgroundColor: MaterialStatePropertyAll(Colors.blue)),
+                  child: const Text('Envoyer'),
+                ),
+              )
+            ],
+          ))),
     ));
   }
 }
