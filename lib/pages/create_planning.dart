85e9917227c2c4eb33d02f07bea5f955d07ef5a0
diambu jeannot <diambujeannot@gmail.com>
2024-05-23 08:24:48 -0700

    create state full widget

--- lib/pages/create_planning.dart
+++ lib/pages/create_planning.dart
@@ -1,14 +1,18 @@
 import 'package:flutter/material.dart';
 
-class CreatePlanning extends StatelessWidget {
+class CreatePlanning extends StatefulWidget {
   const CreatePlanning({super.key});
 
   @override
+  State<CreatePlanning> createState() => _CreatePlanningState();
+}
+
+class _CreatePlanningState extends State<CreatePlanning> {
+  @override
   Widget build(BuildContext context) {
     return const Scaffold(
-      body: Center(
-        child: Text('Create Planning'),
-      ),
-    );
+        body: Center(
+      child: Text("Formulaire"),
+    ));
   }
 }
